abead935269550fd2bdba4e49655f0edd8417961
Anna Sheaffer <annasheaffer@gmail.com>
2014-02-10 13:19:59 -0500

    Add closed bets to home page

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -3,6 +3,7 @@ class SiteController < ApplicationController
     if current_user
       @accepted_bets = Bet.joins(:positions).where(positions: {user_id: current_user.id, status: "accepted"})
       @pending_bets =  Bet.joins(:positions).where(positions: {user_id: current_user.id, status: "pending"})
+      @closed_bets = Bet.joins(:positions).where(positions: {user_id: current_user.id, status: "closed"})
     end
   end
 
