f7322edb1049f18ce906942104c0f2e94ab7d10d
Anna Sheaffer <annasheaffer@gmail.com>
2014-02-28 09:23:51 -0500

    Add user unit test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :users
+
+  test "should not register user without unique email" do
+    user = User.new(
+                    email: "test@test.com",
+                    password: "password",
+                    password_confirmation: "password")
+    assert !user.save
+  end
+
+  test "should not register user without matching password confirmation" do
+    user = User.new(
+                    email: "unique@test.com",
+                    password: "password",
+                    password_confirmation: "notamatch")
+    assert !user.save
+  end
+  
 end
