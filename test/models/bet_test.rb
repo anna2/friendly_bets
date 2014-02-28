425c4c71d06840366478138ba5ee24deaf0547f6
Anna Sheaffer <annasheaffer@gmail.com>
2014-02-28 09:21:50 -0500

    Add bet unit test

--- test/models/bet_test.rb
+++ test/models/bet_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class BetTest < ActiveSupport::TestCase
+  test "bet must have an amount" do
+    bet = Bet.new(title: "title", description: "desc")
+    assert bet.invalid?
+  end
+
   test "bet amount must be positive" do
     bet = Bet.new(title: "Test", description: "test")
     bet.amount = 0
@@ -12,6 +17,11 @@ class BetTest < ActiveSupport::TestCase
 
   test "bet must have title" do
     bet = Bet.new(description: "test", amount: 12)
+    assert bet.invalid?
+  end
+
+  test "bet must have description" do
+    bet = Bet.new(title: "Title", amount: 3)
     assert bet.invalid?
   end
 end
