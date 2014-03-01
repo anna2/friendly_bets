5aa5152ebbfe68cf8488e033f8de20b42156a7ef
Anna Sheaffer <annasheaffer@gmail.com>
2014-03-01 11:37:31 -0500

    Add bets functional tests

--- test/controllers/bets_controller_test.rb
+++ test/controllers/bets_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class BetsControllerTest < ActionController::TestCase
+  fixtures :bets, :users, :positions
 
   setup do
     sign_in users(:one)
@@ -15,7 +16,6 @@ class BetsControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
-    assert_not_nil assigns(:bets)
   end
 
   test "should get new" do
@@ -36,11 +36,6 @@ class BetsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get edit" do
-    get :edit, id: @bet
-    assert_response :success
-  end
-
   test "should update bet" do
     patch :update, id: @bet, bet: { amount: @bet.amount, description: @bet.description, title: @bet.title }
     assert_redirected_to bet_path(assigns(:bet))
@@ -52,5 +47,44 @@ class BetsControllerTest < ActionController::TestCase
     end
 
     assert_redirected_to bets_path
+  end
+
+  test "should show bet participants" do
+    get :close, id: @bet.id
+    assert_response :success
+  end
+
+  test "should update position statuses to closed" do
+    assert_difference(Position.where(status: "closed")) do
+      post :close_2, id: @bet.id, winner: users(:one).email
+    end
+    assert_redirected_to stats_bet_path(assigns(:bet))
+  end
+
+  test "should update 1 position win status to true" do
+    assert_difference Position.where(win: true), +1 do
+      post :close_2, id: @bet.id, winner: users(:one).email
+    end
+    assert_redirected_to stats_bet_path(assigns(:bet))
+  end
+
+  test "should update other position win statuses to false" do
+    assert_difference Position.where(win: false), +1 do
+      post :close_2, id: @bet.id, winner: users(:one).email
+    end
+    assert_redirected_to stats_bet_path(assigns(:bet))
+  end
+
+  test "should update winner in bet record" do
+    assert_difference Bet.where(winner_id: nil), -1 do
+      post :close_2, id: @bet.id, winner: users(:one).email
+    end
+    assert_redirected_to stats_bet_path(assigns(:bet))
+  end
+
+  test "should mail winners and losers" do
+    assert_difference 'ActionMailer::Base.deliveries.size', +2  do
+      post :close_2, id: @bet.id, winner: users(:one).email
+    end
   end
 end
