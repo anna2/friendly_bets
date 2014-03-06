34a2581c20a445a0a7f0f204c4e33772ebda9f8d
Anna Sheaffer <annasheaffer@gmail.com>
2014-03-06 13:28:19 -0500

    Prevent overwriting old positions

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -7,47 +7,51 @@ class PositionsController < ApplicationController
   end
 
   def create
-    # first, delete position record on pending bet
-    if Position.where(bet_id: @bet.id, user_id: current_user.id)
-      Position.where(bet_id: @bet.id, user_id: current_user.id).destroy_all
-    end
-    
-    # now, create position on accepted bet 
-    @position = Position.new(position: params[:position][:position], bet_id: @bet.id, user_id: current_user.id, status: "accepted")
-    respond_to do |format|
-      if @position.save
-        if Position.where(bet_id: @bet.id).size == 1
-          @position.update(admin: true)
-        else
-          @position.update(admin: false)
-        end
-        format.html { redirect_to new_bet_invitation_path(@bet) }
-        format.json { render json: @position, status: :created, location: @position }
+    # do not allow overwriting of old positions
+    if Position.where(bet_id: @bet.id, user_id: current_user.id, status: "accepted").size != 0
+      flash[:error] = "You already have a position on this bet."
+      redirect_to bet_path(@bet)
+    else
+      #handle position on newly-accepted bets
+      if Position.where(bet_id: @bet.id, user_id: current_user.id, status: "pending").size == 1
+        @position = Position.where(bet_id: @bet.id, user_id: current_user.id)
+        @position.update(position: params[:position][:position])
+        @position.update(status: "accepted")
+        @position.update(admin: false)
       else
-        format.html { render action: "new" }
-        format.json { render json: @position.errors, status: :unprocessable_entity }
+        # handle position on newly-created bet
+        @position = Position.new(position: params[:position][:position], bet_id: @bet.id, user_id: current_user.id, status: "accepted", admin: true)
+      end
+      respond_to do |format|
+        if @position.save
+          format.html { redirect_to new_bet_invitation_path(@bet) }
+          format.json { render json: @position, status: :created, location: @position }
+        else
+          format.html { render action: "new" }
+          format.json { render json: @position.errors, status: :unprocessable_entity }
+        end
       end
     end
+    end
+
+    def destroy
+      @position = Position.find(params[:id])
+      @position.destroy
+      redirect_to bets_path
+    end
+
+    private
+
+    def set_bet
+      @bet = Bet.find(params[:bet_id])
+    end
+
+    def set_position
+      @position = Position.find(params[:id])
+    end
+
+    def pos_params
+      params.require(:position)
+    end
+
   end
-
-  def destroy
-    @position = Position.find(params[:id])
-    @position.destroy
-    redirect_to bets_path
-  end
-
-  private
-
-  def set_bet
-    @bet = Bet.find(params[:bet_id])
-  end
-
-  def set_position
-    @position = Position.find(params[:id])
-  end
-
-  def pos_params
-    params.require(:position)
-  end
-
-end
