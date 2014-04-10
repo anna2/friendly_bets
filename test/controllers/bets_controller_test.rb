81770b229caf7fc68f30802d0018a81666184209
Anna Sheaffer <annasheaffer@gmail.com>
2014-04-10 09:06:38 -0400

    Update test to reflect moving find() from controller to model

--- test/controllers/bets_controller_test.rb
+++ test/controllers/bets_controller_test.rb
@@ -58,28 +58,28 @@ class BetsControllerTest < ActionController::TestCase
     assert_difference(Position.where(status: "closed")) do
       post :close_2, id: @bet.id, winner: users(:one).email
     end
-    assert_redirected_to stats_bet_path(assigns(:bet))
+    assert_redirected_to stats_path
   end
 
   test "should update 1 position win status to true" do
     assert_difference Position.where(win: true), +1 do
       post :close_2, id: @bet.id, winner: users(:one).email
     end
-    assert_redirected_to stats_bet_path(assigns(:bet))
+    assert_redirected_to stats_path
   end
 
   test "should update other position win statuses to false" do
     assert_difference Position.where(win: false), +1 do
       post :close_2, id: @bet.id, winner: users(:one).email
     end
-    assert_redirected_to stats_bet_path(assigns(:bet))
+    assert_redirected_to stats_path
   end
 
   test "should update winner in bet record" do
     assert_difference Bet.where(winner_id: nil), -1 do
       post :close_2, id: @bet.id, winner: users(:one).email
     end
-    assert_redirected_to stats_bet_path(assigns(:bet))
+    assert_redirected_to stats_path
   end
 
   test "should mail winners and losers" do
